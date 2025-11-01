290f1a14729033e6b08ee0fe1f81aa6197e64815
codedvalley <codedvalley@gmail.com>
2025-11-01 22:24:55 +0100

    oefeningen gedaan

---
+++ dictionaries.py
@@ -0,0 +1,140 @@
+# ==========================================
+# Voorbeeld Opdracht
+#
+# Maak een dictionary genaamd 'fruit' met daarin de sleutels 'appel' en 'banaan'.
+# Geef 'appel' de value 'rood' en 'banaan' de value 'geel'.
+# Print vervolgens de kleur van de banaan door de naam 'banaan' te gebruiken.
+#
+# Verwachte uitkomst:  geel
+# ==========================================
+from logging import exception
+
+# dictionary met kleuren als values
+fruit = {'appel': 'rood', 'banaan': 'geel'}
+
+# print de kleur van de banaan
+print(fruit['banaan'])  # Het resultaat is: geel
+
+
+# ==========================================
+# Opdracht 1:
+# Gegeven is de dictionary 'boodschappen' met daarin de keys 'Appels' en 'Brood'.
+# Maak de volgende deelopdrachten. Print de uitkomst van elke opdracht om te zien of je het juiste resultaat krijgt.
+#
+# Opdracht 1a:
+# Roep de waarde aan die hoort bij de naam 'Brood'
+# Verwachte uitkomst: 2
+#
+# Opdracht 1b:
+# Gebruik de get() methode op de waarde 'Appels' en 'Bananen'. Zorg dat als de naam niet bestaat, de tekst 'Niet gevonden' wordt geprint.
+# ==========================================
+
+boodschappen = {'Appels': 6, 'Brood': 2}
+print(boodschappen['Brood'])
+
+
+
+
+print(boodschappen.get('Appels', 'Niet gevonden'))
+print(boodschappen.get('Bananen', 'Niet gevonden'))
+
+
+# ==========================================
+# Opdracht 2:
+# Gegeven zijn vier boodschappenlijstjes als dictionary. Je kan een if statement gebruiken en de == operator om te vergelijken.
+#
+# Opdracht 2a:
+# Vergelijk de boodschappenlijstjes van Marie en Raj. Als de lijstjes gelijk zijn, print 'De boodschappenlijstjes zijn gelijk.'.
+#
+# Opdracht 2b:
+# Vergelijk de boodschappenlijstjes (Marie, Raj en Jan). Als de lijstjes gelijk zijn, print 'De boodschappenlijstjes zijn gelijk.'.
+# Waarom worden deze lijstjes wel/niet als gelijk beschouwd?
+# keys hebben dezelfde waarde
+#
+# Opdracht 2c:
+# Vergelijk nu op dezelfde manier alle lijstjes. Zijn ze gelijk? Waarom wel/niet?
+# ==========================================
+
+boodschappenlijst_marie = {'Brood': 1, 'Appels': 6}
+boodschappenlijst_raj = {'Brood': 1, 'Appels': 6}
+boodschappenlijst_jan = {'Appels': 6, 'Brood': 1}
+boodschappenlijst_karel = {'Appels': 6, 'Brood': 25}
+
+if boodschappenlijst_marie == boodschappenlijst_raj == boodschappenlijst_jan :
+    print('De boodschappenlijstjes zijn gelijk.')
+else:
+    print('niet gelijk')
+
+
+# ==========================================
+# Opdracht 2:
+# De voorraad van meubelwinkel 'Op Eigen Houtje' is als volgt:
+# Gegeven is de dictionary 'meubels' met daarin de keys 'banken' en 'stoelen'.
+# Maak de volgende deelopdrachten. Print de uitkomst van elke opdracht om te zien of je het juiste resultaat krijgt.
+#
+# Opdracht 2a:
+# Er worden 6 banken verkocht. Pas de waarde van de naam 'banken' aan en print de aangepaste dictionary
+# Verwachte uitkomst: {'banken': 4, 'stoelen': 20}
+
+# Opdracht 2b:
+# Er komen klachten dat klanten door eerder gekochte stoelen zijn gezakt. De winkel besluit om de stoelen uit het assortiment te halen.
+# Verwijder de naam 'stoelen' (en daarmee ook de bijbehorende value) uit de dictionary en print de aangepaste dictionary.
+# Verwachte uitkomst: {'banken': 4}
+
+# Opdracht 2c:
+# De winkel gaat tafels verkopen en koopt er gelijk 15 in. Voeg de naam 'tafels' toe met een waarde van 15 en print de aangepaste dictionary.
+# Verwachte uitkomst: {'banken': 4, 'tafels': 15}
+# ==========================================
+
+meubels = {'banken': 10, 'stoelen': 20}
+meubels.update({'banken' : 4})
+print(meubels)
+
+del meubels ['stoelen']
+print(meubels)
+
+meubels.update({'tafels': 15})
+print(meubels)
+
+# ==========================================
+# Opdracht 3:
+# Docent Erik heeft proefwerken van studenten nagekeken en de cijfers in een dictionary opgeslagen.
+# Erik komt erachter dat hij een fout heeft gemaakt en moet de cijfers met een factor 1,5 vermenigvuldigen.
+# Met als enige uitzondering dat een cijfer nooit hoger mag zijn dan een 10.
+# Print de uitkomst om te zien of je het juiste resultaat krijgt.
+
+# Gegeven is de dictionary 'cijfers' met daarin de keys 'Jaap', 'Winnie', 'Jeroen' en 'Lana' en hun cijfers.
+# Verwachte uitkomst: {'Jaap': 4.5, 'Winnie': 6.0, 'Jeroen': 10, 'Lana': 10}
+# ==========================================
+
+cijfers = {'Jaap': 3, 'Winnie': 4, 'Jeroen': 9, 'Lana': 10}
+
+for naam in cijfers:
+    if cijfers[naam] < 10:
+        cijfers[naam] *= 1.5
+
+print(cijfers)
+
+
+
+# ==========================================
+# Opdracht 4:
+# De eigenaar van dierentuin 'Het Zootje' heeft een lijst met dieren en hun aantallen. De dieren en hun aantallen zijn opgeslagen in een dictionary.
+# Maak onderstaande deelopdrachten.
+#
+# Opdracht 4a:
+# De eigenaar wil de dieren op alfabetische volgorde zien. Sorteer de dieren op naam en print de dictionary uit.
+# Verwachte uitkomst: {'Aap': 5, 'Giraffe': 2, 'Leeuw': 3, 'Olifant': 4, 'Zebra': 1}
+#
+# Opdracht 4b:
+# Nu wil de eigenaar in een oogopslag wat het hoogste aantal is en het laagste. Sorteer nu de aantallen van hoog naar laag en print de dictionary uit. (De keys hoef je niet te printen)
+# ==========================================
+
+dieren = {'Olifant': 5, 'Zebra': 8, 'Aap': 12, 'Giraffe': 3, 'Neushoorn': 7}
+dieren = tuple(sorted(dieren.items()))
+print(dieren)
+
+dieren = {'Olifant': 5, 'Zebra': 8, 'Aap': 12, 'Giraffe': 3, 'Neushoorn': 7}
+
+dieren = tuple(sorted(dieren.values()))
+print(dieren)
---
+++ sets.py
@@ -0,0 +1,92 @@
+# ==========================================
+# Voorbeeld Opdracht
+# Gegeven is een set met de getallen 1, 2 en 3.
+# Voeg het getal 1 toe aan de set en daarna het getal 4.
+#
+# Print de set 'getallen'.
+# Het getal 1 stond al in de set, dus wordt niet nogmaals toegevoegd. Het getal 4 wordt wel toegevoegd.
+# ==========================================
+
+# set met getallen
+getallen = {1, 2, 3}
+
+# getal 1 en 4 worden toegevoegd aan de set
+getallen.add(1)
+getallen.add(4)
+
+print('getallen: ', getallen)  # Het resultaat is: {1, 2, 3, 4}
+
+
+# ==========================================
+# Opgave 1:
+# Gegeven de verzameling {3, 44, 17, 23, 58, 9, 36}
+# Voer de volgende opdrachten uit:
+# - Voeg de value 27 aan de verzameling toe.
+# - Verwijder de value 23 uit de verzameling.
+# - Druk alle values in de verzameling tussen 20 en 50 af. Gebruik hiervoor een for loop.
+#
+# Verwachte uitkomst: 36, 27, 44
+# ==========================================
+
+verzameling = {3, 44, 17, 23, 58, 9, 36}
+
+#     Voegt de value 27 aan de verzameling toe.
+verzameling.add(27)
+#     Verwijdert de value 23 uit de verzameling.
+verzameling.remove(23)
+#     Loopt door alle values in 'verzameling' en drukt de values af die tussen 20 en 50 liggen.
+#     In de if wordt er gecheckt of de linker value kleiner is dan de rechter value. Dus is 20 minder dan 'i' EN is 'i' minder dan 50.
+for i in verzameling:
+    if 20 < i < 50:
+        print(i)
+
+
+
+# ==========================================
+# Opgave 2:
+# Gegeven de verzamelingen {red, blue, green} en {yellow, blue, green}
+# Zoek uit met behulp van wiskundige verzamelingsoperatoren of methodes:
+# - Welke kleur zit wel in verzameling_kleuren_1 maar niet in verzameling_kleuren_2?
+# - Welke kleuren zitten niet in beide sets? ('red' en 'yellow')
+#
+# Print de resultaten.
+# ==========================================
+
+verzameling_kleuren_1 = {'red', 'blue', 'green'}
+verzameling_kleuren_2 = {'yellow', 'blue', 'green'}
+
+#   checkt of de kleuren in verzameling_kleuren_1 zitten maar niet in verzameling_kleuren_2
+print(verzameling_kleuren_1 - verzameling_kleuren_2)
+print(verzameling_kleuren_1.difference(verzameling_kleuren_2))
+#   checkt welke kleuren niet in beide sets zitten
+print(verzameling_kleuren_1 ^ verzameling_kleuren_2)
+print(verzameling_kleuren_1.symmetric_difference(verzameling_kleuren_2))
+
+
+
+# ==========================================
+# Opgave 3:
+# Gegeven de verzamelingen {11, 22, 33} en {5, 11, 16, 22}
+# Gebruik wiskundige verzamelingsoperatoren om de volgende verzamelingen te printen:
+#     {33}
+#     {5, 16}
+#     {5, 11, 16, 22, 33}
+#     {11, 22}
+#
+# (LET OP: De volgorde van de values in de verzamelingen is niet belangrijk, die kan namelijk veranderen omdat een set unordered is.)
+# ==========================================
+
+verzameling1 = {11, 22, 33}
+verzameling2 = {5, 11, 16, 22}
+
+#     Haalt de values uit verzameling1 die niet in verzameling2 zitten.
+print(verzameling1 - verzameling2)  # Het resultaat is: {33}
+
+#     Haalt de values uit verzameling2 die niet in verzameling1 zitten.
+print(verzameling2 - verzameling1)  # De getallen in de set zijn: 5, 16
+
+#     Voegt de values van verzameling1 en verzameling2 samen.
+print(verzameling1 | verzameling2)  # De getallen in de set zijn: 33, 5, 11, 16, 22
+
+#     Haalt de values uit verzameling1 en verzameling2 die in beide sets zitten.
+print(verzameling1 & verzameling2)  # De getallen in de set zijn: 11, 22
